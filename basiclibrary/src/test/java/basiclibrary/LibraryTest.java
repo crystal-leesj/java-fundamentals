b5d71379075a20aa66ec356eb0e92a5015125bcc
Crystal Soo Lee <soo.leesj@gmail.com>
2020-01-09 23:58:13 -0800

    created function tally

--- basiclibrary/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/src/test/java/basiclibrary/LibraryTest.java
@@ -5,7 +5,8 @@ package basiclibrary;
 
 import org.junit.Test;
 
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -55,13 +56,34 @@ public class LibraryTest {
         assertEquals(expected, actual);
     }
 
-    @Test public void testAnalyzeWeatherDataMethod() {
+    @Test public void testAnalyzeWeather_checkUniqueTemperatures() {
         int[][] weeklyMonthTemperatures = {
                 {66, 64, 58, 65, 71, 57, 60},
                 {57, 65, 65, 70, 72, 65, 51},
                 {55, 54, 60, 53, 59, 57, 61},
                 {65, 56, 55, 52, 55, 62, 57}
         };
-        Library.analyzeWeatherData(weeklyMonthTemperatures);
+        String actual = Library.analyzeWeather(weeklyMonthTemperatures);
+        String expected = "\nNever saw temperature: 63\nNever saw temperature: 67\nNever saw temperature: 68\nNever saw temperature: 69";
+        assertEquals(expected, actual);
     }
+
+    @Test public void testTally_result() {
+        List<String> votes = new ArrayList<>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+
+        String actual = Library.tally(votes);
+        String expected = "Bush received the most votes!";
+        assertEquals(expected, actual);
+    }
+
+
 }
