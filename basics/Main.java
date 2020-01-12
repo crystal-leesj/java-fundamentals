3554b95b62edba9cf7c4983c48607e56b10a66d0
Crystal Soo Lee <soo.leesj@gmail.com>
2020-01-12 00:47:29 -0800

    finish lab 01

--- basics/Main.java
+++ basics/Main.java
@@ -11,8 +11,8 @@ public class Main {
     int turtleCount = 0;
     System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
 
-    flipNHeads(1);
-    // flipNHeads(10);
+    // flipNHeads(1);
+    flipNHeads(6);
 
     Main object = new Main();
     object.clock();
@@ -33,20 +33,19 @@ public class Main {
 
   // Write a function called flipNHeads that accepts an integer n and flips coins until n heads are flipped in a row. Simulate coin flipping by choosing a random number between 0 and 1. Numbers below .5 are considered tails. Numbers at and above .5 are considered heads. Print out heads or tails on one line for each flip. Print It took FLIPS flips to flip N heads in a row. once n heads have been in a row.
   public static void flipNHeads(int n) {
-    double rand = Math.random();
     int flipCounter = 0;
-    int headsCounter = n;
+    int headsCounter = 0;
 
-    while (headsCounter != 0) {
+    while (headsCounter < n) {
+      double rand = Math.random();
       if (rand > 0.5) {
         System.out.println(String.format("heads"));
-        flipCounter++;
-        headsCounter--;
+        headsCounter++;
       } else {
         System.out.println(String.format("tails"));
-        flipCounter++;
-        rand = Math.random();
+        headsCounter = 0;
       }
+      flipCounter++;
     }
 
     System.out.println(String.format("It took %d flips to flip %d head in a row.", flipCounter, n));
